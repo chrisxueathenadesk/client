0b1d3796aeaf322cfd699ef206af37a4327b9f9a
Sayem <afm.sayem@gmail.com>
2017-03-06 00:54:27 +0600

    feat(search-bar): add global search bar element

--- src/resources/elements/index.js
+++ src/resources/elements/index.js
@@ -1,7 +1,9 @@
 const elements = [
   'filter/filter',
   'navbar/navbar',
-  'signin/signin'
+  'signin/signin',
+  'product-card/product-card',
+  'searchbar/searchbar'
 ];
 
 export default elements.map(elem => `./elements/${elem}`);
