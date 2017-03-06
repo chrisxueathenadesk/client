84799a6e3fbb85826af263f164a0f0965e2f13fc
Sayem <afm.sayem@gmail.com>
2017-03-06 14:38:24 +0600

    feat(search): take to search results page when the user searches

--- src/resources/elements/searchbar/searchbar.js
+++ src/resources/elements/searchbar/searchbar.js
@@ -1,3 +1,13 @@
-export class Searchbar {
+import {inject} from 'aurelia-framework';
+import {Router} from 'aurelia-router';
 
+@inject(Router)
+export class Searchbar {
+  constructor(router) {
+    this.router = router;
+  }
+
+  showResults() {
+    this.router.navigateToRoute('search', {search: this.query});
+  }
 }
