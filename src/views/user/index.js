3777418e4906819463f4590510bbe5dc4f66e780
Sayem <afm.sayem@gmail.com>
2017-03-27 13:34:47 +0600

    feat(orders): scaffold routes for orders

--- src/views/user/index.js
+++ src/views/user/index.js
@@ -4,8 +4,9 @@ export class UserRouter {
   configureRouter(config, router) {
     config.map([
       { route: '', redirect: 'requests' },
-      {route: '/trips', moduleId: './trips/index', nav: true, title: 'Trips'},
-      {route: '/requests', moduleId: './requests/index', nav: true }
+      {route: 'trips', moduleId: './trips/index', nav: true, title: 'Trips'},
+      {route: 'requests', moduleId: './requests/index', nav: true },
+      {route: 'shops', moduleId: './shops/index', nav: true }
     ]);
   }
 }
