dfac9cb3d2ec4f9dbe7b974db9b584b04d69d39b
AFM Sayem <afm.sayem@gmail.com>
2017-04-14 11:09:25 +0600

    chore(stripe token): add stripe token to client

--- aurelia_project/environments/dev.js
+++ aurelia_project/environments/dev.js
@@ -1,5 +1,6 @@
 export default {
   debug: true,
   testing: true,
-  base: 'http://localhost:3000/'
+  base: 'http://localhost:3000/',
+  stripe_key: 'pk_test_y6c6G7emK6M5t1tfURhHiWQz'
 };
--- aurelia_project/environments/prod.js
+++ aurelia_project/environments/prod.js
@@ -1,5 +1,6 @@
 export default {
   debug: false,
   testing: false,
-  base: 'https://api.novelship.com/'
+  base: 'https://api.novelship.com/',
+  stripe_key: 'pk_test_y6c6G7emK6M5t1tfURhHiWQz'
 };
