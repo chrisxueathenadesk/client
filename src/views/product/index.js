4d8d01c6463c22c02afa1a3b9a4813496bb0e15a
AFM Sayem <afm.sayem@gmail.com>
2017-04-14 11:34:01 +0600

    feat(acknowledge): add acknowledge page for successful payment

--- src/views/product/index.js
+++ src/views/product/index.js
@@ -2,7 +2,8 @@ export class ProductRouter {
   configureRouter(config, router) {
     config.map([
       {route: '', moduleId: './product'},
-      {route: 'checkout', name: 'checkout', moduleId: './checkout'}
+      {route: 'checkout', name: 'checkout', moduleId: './checkout'},
+      {route: 'acknowledge', name: 'acknowledge', moduleId: './acknowledge'}
     ]);
   }
 }
