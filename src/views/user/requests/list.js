ce38055a47be8034172da4bdad19db21737fd89a
AFM Sayem <afm.sayem@gmail.com>
2017-04-21 23:07:30 +0600

    feat(user request details): fetch and show data for a users own request

--- src/views/user/requests/list.js
+++ src/views/user/requests/list.js
@@ -1,2 +1,20 @@
+import {inject} from 'aurelia-framework';
+import {Api} from '~/models/api';
+
+@inject(Api)
 export class RequestListVM {
+  requests = {
+    params: {
+      include: ['product']
+    }
+  }
+  constructor(api) {
+    this.api = api;
+  }
+
+  activate(params) {
+    this.api.fetch('me/requests', this.requests.params)
+      .then(requests => this.requests.data = requests.results)
+      .catch(err => console.log(err));
+  }
 }
