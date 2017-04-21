a2b7d5d9191515c1553348bf845aee76ed27d425
AFM Sayem <afm.sayem@gmail.com>
2017-04-21 23:09:06 +0600

    feat(product list): show the products of a shop

--- src/views/user/shops/index.js
+++ src/views/user/shops/index.js
@@ -4,9 +4,11 @@ export class ShopRouter {
   configureRouter(config, router) {
     config.map([
       { route: '', redirect: 'products'},
-      { route: 'products', name: 'shopProducts', moduleId: './products/index', title: 'Products' },
-      { route: 'requests', name: 'shopRequests', moduleId: './requests/index' }
+      { route: 'products', name: 'shopProducts', moduleId: './products/index', nav: true, title: 'Products' },
+      { route: 'requests', name: 'shopRequests', moduleId: './requests/index', nav: true, title: 'Orders' }
     ]);
+
+    this.router = router;
   }
 }
 
--- src/views/user/shops/products/list.js
+++ src/views/user/shops/products/list.js
@@ -1,5 +1,22 @@
+import {Api} from '~/models/api';
+import {inject} from 'aurelia-framework';
+
+@inject(Api)
 export class ShopProductListVM {
-  constructor() {
+  shop = {};
+  products = {};
+  constructor(api) {
+    this.api = api;
+  }
+
+  activate(params) {
+    this.api.fetch(`me/shops/${params.shop_id}`)
+      .then(shop => this.shop.data = shop)
+      .catch(err => console.log(err));
+
+    this.api.fetch(`me/shops/${params.shop_id}/products`)
+      .then(products => this.products.data = products.results)
+      .catch(err => console.log(err));
   }
   // two navs trips and requests
 }
