c5abe28f11c0ae5bc8e98c5934548c2a48970577
AFM Sayem <afm.sayem@gmail.com>
2017-04-24 11:49:07 +0600

    feat(redirect): redirect to login page when unauthenticated user tries to place order

--- src/views/product/index.js
+++ src/views/product/index.js
@@ -2,7 +2,7 @@ export class ProductRouter {
   configureRouter(config, router) {
     config.map([
       {route: '', moduleId: './product'},
-      {route: 'checkout', name: 'checkout', moduleId: './checkout'},
+      {route: 'checkout', name: 'checkout', auth: true, moduleId: './checkout'},
       {route: 'acknowledge', name: 'acknowledge', moduleId: './acknowledge'}
     ]);
   }
