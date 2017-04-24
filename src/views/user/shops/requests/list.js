fdca7b69477004e2632951875540859dbe67cb6c
AFM Sayem <afm.sayem@gmail.com>
2017-04-24 12:08:54 +0600

    fix(shop requests): take to request details instead of product page

--- src/views/user/shops/requests/list.js
+++ src/views/user/shops/requests/list.js
@@ -8,11 +8,15 @@ export class ShopRequestListVM {
       include: ['product', 'customer']
     }
   }
+  shop = {};
   constructor(api) {
     this.api = api;
   }
 
   activate(params) {
+    this.api.fetch(`me/shops/${params.shop_id}`)
+      .then(shop => this.shop.data = shop)
+      .catch(err => console.log(err));
     this.api.fetch(`me/shops/${params.shop_id}/requests`, this.requests.params)
       .then(requests => this.requests.data = requests.results)
       .catch(err => console.log(err));
