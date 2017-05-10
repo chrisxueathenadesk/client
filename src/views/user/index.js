61dda36b551992b86688fd73dd933ca875e4832a
AFM Sayem <afm.sayem@gmail.com>
2017-05-10 14:51:56 +0600

    feat(tabs): add tab based navigation for user dashboard

--- src/views/user/index.js
+++ src/views/user/index.js
@@ -3,11 +3,15 @@ export class UserRouter {
 
   configureRouter(config, router) {
     config.map([
-      {route: '', moduleId: './dashboard'},
-      {route: 'trips', moduleId: './trips/index'},
+      {route: '', redirect: 'dashboard'},
+      {route: 'dashboard', name: 'dashboard', title: 'Dashboard', nav: true, moduleId: './dashboard'},
+      {route: 'profile', name: 'profile', title: 'Edit Profile', nav: true, moduleId: './profile/profile'},
+      {route: 'trips', name: '', moduleId: './trips/index'},
       {route: 'requests', moduleId: './requests/index'},
       {route: 'shops/:shop_id', moduleId: './shops/index'}
     ]);
+
+    this.router = router;
   }
 }
 
