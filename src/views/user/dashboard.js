ef8218a2e093a9ea46faee455f4ba278bd1eeee6
AFM Sayem <afm.sayem@gmail.com>
2017-05-10 14:53:57 +0600

    feat(dashboard): restructure user dashboard

--- src/views/user/dashboard.js
+++ src/views/user/dashboard.js
@@ -2,12 +2,10 @@ import {inject} from 'aurelia-framework';
 import {Router} from 'aurelia-router';
 import {Api} from '~/models/api';
 import {constants} from '~/services/constants';
-import {Payment} from '~/services/payment';
 import {UserService} from '~/services/user';
 import {AuthService} from 'aurelia-auth';
-import {ExternalHttp} from '~/services/external-http';
 
-@inject(Api, Router, UserService, AuthService, ExternalHttp, Payment)
+@inject(Api, Router, UserService, AuthService)
 export class DashboardView {
   requests = {
     params: {
@@ -19,59 +17,12 @@ export class DashboardView {
     statuses: constants.requestStatus
   };
   shops = {};
-  countries = {};
-  constructor(api, router, userStore, auth, http, payment) {
+
+  constructor(api, router, userStore, auth) {
     this.api = api;
     this.router = router;
     this.user = userStore.user;
     this.auth = auth;
-    this.http = http;
-    this.payment = payment;
-  }
-
-  userUpdate(fragment) {
-    return this.api
-      .edit('me', fragment)
-      .then(success =>this.user = Object.assign(this.user, fragment))
-      .catch(err => console.log(err));
-  }
-
-  upload() {
-    let avatarUrl;
-    this.getUploadUrl(this.avatar[0], 'avatar')
-      .then((response) => {
-        avatarUrl = response.signed_request.split('?')[0];
-        return this.http.fetch(response.signed_request, {
-          method: 'PUT',
-          body: this.avatar[0]
-        });
-      })
-      .then(success => this.userUpdate({avatar: avatarUrl}))
-      .catch(err => console.log(err));
-  }
-
-  getRequests() {
-    console.log(this.cards);
-    setTimeout(() => {
-      this.api
-        .fetch('me/requests', this.requests.params)
-        .then(requests => this.requests.data = requests.results)
-        .catch(err => this.requests.error = err);
-    });
-    /* required for event propagation */
-    return true;
-  }
-
-  removeCard(cardId) {
-    this.payment.deleteCard(cardId)
-      .then(() => this.payment.getCards())
-      .then(cards => this.cards = cards.data)
-      .catch(err => console.log(err));
-  }
-
-  getUploadUrl(file, type) {
-    return this.api
-      .fetch('upload', {file_name: file.name, folder_name: type, file_type: file.type});
   }
 
   activate() {
@@ -84,16 +35,17 @@ export class DashboardView {
       .fetch('me/shops')
       .then(shops => this.shops.data = shops.results)
       .catch(err => this.shops.error = err);
+  }
 
-    this.payment
-      .getCards()
-      .then(cards => this.cards = cards.data)
-      .catch(err => console.log(err));
-
-    this.api
-      .fetch('countries')
-      .then(countries => this.countries.data = countries.results)
-      .catch(err => this.countries.error = err);
+  getRequests() {
+    setTimeout(() => {
+      this.api
+        .fetch('me/requests', this.requests.params)
+        .then(requests => this.requests.data = requests.results)
+        .catch(err => this.requests.error = err);
+    });
+    /* required for event propagation */
+    return true;
   }
 }
 
