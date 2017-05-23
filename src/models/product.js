8cd587088650111ca982d53f0a5a759686466cd5
AFM Sayem <afm.sayem@gmail.com>
2017-05-23 11:41:13 +0800

    feat(product): add model for product creation validation

---
+++ src/models/product.js
@@ -0,0 +1,43 @@
+import {ValidationRules} from 'aurelia-validation';
+
+export class Product {
+  name;
+  category_id;
+  brand_id;
+  source_id;
+  order_count;
+  preorder;
+  featured;
+  price;
+  weight;
+  dimensions;
+  platform_charge;
+  delivery_time;
+  colors;
+  editions;
+  sizes;
+  gallery;
+  currency;
+  url;
+  description;
+}
+
+ValidationRules
+  .ensure(product => product.category_id)
+    .required()
+  .ensure(product => product.name)
+    .required()
+  .ensure(product => product.source_id)
+    .required()
+  .ensure(product => product.delivery_time)
+    .displayName('Delivery time')
+    .required()
+  .ensure(product => product.description)
+    .minLength(100)
+    .required()
+  .ensure(product => product.price)
+    .required()
+  .ensure(product => product.currency)
+    .required()
+  .on(Product);
+
