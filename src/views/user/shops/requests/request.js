e145580bcd57a130c18e3a4e23fda4f6f39f60fa
AFM Sayem <afm.sayem@gmail.com>
2017-05-23 16:10:18 +0800

    feat(order status): let merchants change order status

--- src/views/user/shops/requests/request.js
+++ src/views/user/shops/requests/request.js
@@ -1,5 +1,7 @@
 import {inject} from 'aurelia-framework';
 import {Api} from '~/services/api';
+import {notify} from '~/services/notification';
+import {constants} from '~/services/constants';
 
 @inject(Api)
 export class ShopRequestVM {
@@ -8,17 +10,34 @@ export class ShopRequestVM {
       include: ['product', 'source', 'destination', 'customer']
     }
   };
+  editRequest = {};
+  statuses = constants.requestStatus.concat('canceled');
   constructor(api) {
     this.api = api;
   }
 
   activate(params) {
-    this.api.fetch(`me/shops/${params.shop_id}/requests/${params.request_id}`, this.request.params)
+    this.params = params;
+    this.api
+      .fetch(`me/shops/${params.shop_id}/requests/${params.request_id}`, this.request.params)
       .then(response => {
         this.request.data = response;
+        this.editRequest.status = response.status;
       })
       .catch(err => {
         console.log(err);
+      });
+  }
+
+  update(fragment) {
+    this.api
+      .edit(`me/shops/${this.params.shop_id}/requests/${this.params.request_id}`, fragment)
+      .then(response => {
+        Object.assign(this.request.data, fragment);
+        notify().log('Successfully updated!');
+      })
+      .catch(err => {
+        notify().log('Update failed!');
       });
   }
 }
