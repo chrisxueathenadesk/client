0bda5db3bb13dbdfbfe4c9de0a89508facb6152c
AFM Sayem <afm.sayem@gmail.com>
2017-05-28 09:37:24 +0800

    feat(reset): reset password view and controller

--- src/views/auth/reset.js
+++ src/views/auth/reset.js
@@ -1,9 +1,18 @@
 import {inject} from 'aurelia-framework';
 import {Api} from '~/services/api';
+import {Router} from 'aurelia-router';
 
-@inject(Api)
+@inject(Api, Router)
 export class Reset {
-  constructor() {
+  constructor(api, router) {
+    this.api = api;
+    this.router = router;
+  }
 
+  reset() {
+    this.api
+    .create('auth/request-reset', {email: this.email})
+    .then(success => this.router.navigateToRoute('confirm'))
+    .catch(err => console.log(err));
   }
 }
