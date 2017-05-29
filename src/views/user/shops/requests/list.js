06dcff27d4d71824156a0228c48930674c4bc9b3
AFM Sayem <afm.sayem@gmail.com>
2017-05-29 11:51:09 +0800

    feat(order filters): add ability to filter orders by status, date and product

--- src/views/user/shops/requests/list.js
+++ src/views/user/shops/requests/list.js
@@ -1,24 +1,38 @@
 import {inject} from 'aurelia-framework';
 import {Api} from '~/services/api';
+import {constants} from '~/services/constants';
 
 @inject(Api)
 export class ShopRequestListVM {
   requests = {
     params: {
-      include: ['product', 'customer']
+      include: ['product', 'customer'],
+      filter: {
+        'status:eq': 'pending'
+      }
     }
   }
   shop = {};
+  products = {};
   constructor(api) {
     this.api = api;
+    this.statuses = constants.requestStatus;
+  }
+
+  getOrders() {
+    this.api.fetch(`me/shops/${this.params.shop_id}/requests`, this.requests.params)
+      .then(requests => this.requests.data = requests.results)
+      .catch(err => console.log(err));
   }
 
   activate(params) {
+    this.params = params;
+    this.getOrders();
     this.api.fetch(`me/shops/${params.shop_id}`)
       .then(shop => this.shop.data = shop)
       .catch(err => console.log(err));
-    this.api.fetch(`me/shops/${params.shop_id}/requests`, this.requests.params)
-      .then(requests => this.requests.data = requests.results)
+    this.api.fetch(`me/shops/${params.shop_id}/products`)
+      .then(products => this.products.data = products.results)
       .catch(err => console.log(err));
   }
 }
