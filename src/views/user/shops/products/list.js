00fc9f13116f62e2f82394c3e7a7a7deefb3dec4
AFM Sayem <afm.sayem@gmail.com>
2017-05-29 14:55:37 +0800

    feat(product list): add filters to product list

--- src/views/user/shops/products/list.js
+++ src/views/user/shops/products/list.js
@@ -4,20 +4,40 @@ import {inject} from 'aurelia-framework';
 @inject(Api)
 export class ShopProductListVM {
   shop = {};
-  products = {};
+  products = {
+    params: {
+      filter: {}
+    }
+  };
+  countries = {};
+  categories = {};
   constructor(api) {
     this.api = api;
   }
 
+  getProducts() {
+    this.api.fetch(`me/shops/${this.params.shop_id}/products`, this.products.params)
+      .then(products => this.products.data = products.results)
+      .catch(err => console.log(err));
+  }
+
   activate(params) {
+    this.params = params;
+    this.getProducts();
+
     this.api.fetch(`me/shops/${params.shop_id}`)
       .then(shop => this.shop.data = shop)
       .catch(err => console.log(err));
 
-    this.api.fetch(`me/shops/${params.shop_id}/products`)
-      .then(products => this.products.data = products.results)
+    this.api
+      .fetch('countries')
+      .then(data => this.countries.data = data.results)
+      .catch(err => console.log(err));
+
+    this.api
+      .fetch('categories')
+      .then(data => this.categories.data = data.results)
       .catch(err => console.log(err));
   }
-  // two navs trips and requests
 }
 
