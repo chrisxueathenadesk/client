4724798dc8f7cd7a0f84a96513622be17d987d41
AFM Sayem <afm.sayem@gmail.com>
2017-06-05 15:14:17 +0800

    chore(config): add stripe live key

--- aurelia_project/environments/prod.js
+++ aurelia_project/environments/prod.js
@@ -2,5 +2,5 @@ export default {
   debug: false,
   testing: false,
   base: 'https://api.novelship.com/',
-  stripe_key: 'pk_test_y6c6G7emK6M5t1tfURhHiWQz'
+  stripe_key: 'pk_live_ZSoVkfqE1IhCf6OJP9HtScJ4'
 };
