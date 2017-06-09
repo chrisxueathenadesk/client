ffbc21f50307fc89f6f995e54842e5895242cbfa
AFM Sayem <afm.sayem@gmail.com>
2017-06-09 18:20:39 +0800

    feat(embeds): support markdown embed

--- src/resources/value-converters/markdown.js
+++ src/resources/value-converters/markdown.js
@@ -2,7 +2,7 @@ import  MarkdownIt from 'markdown-it';
 
 export class MarkdownValueConverter {
   constructor() {
-    this.md = new MarkdownIt();
+    this.md = new MarkdownIt({html: true});
   }
 
   toView(text) {
