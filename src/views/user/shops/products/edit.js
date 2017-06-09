cf48ed4fb9034f176a665090bc3b1464c769a08a
AFM Sayem <afm.sayem@gmail.com>
2017-06-09 18:33:20 +0800

    feat(edit): allow merchants to edit product

--- src/views/user/shops/products/edit.js
+++ src/views/user/shops/products/edit.js
@@ -1,2 +1,46 @@
-export class EditProduct {
+import {inject} from 'aurelia-framework';
+import {Api} from '~/services/api';
+import {notify} from '~/services/notification';
+import {utilities} from '~/services/utilities';
+
+@inject(Api)
+export class EditProductView {
+  constructor(api) {
+    this.api = api;
+  }
+
+  activate(params) {
+    this.params = params;
+    this.api
+      .fetch(`products/${params.product_id}`)
+      .then(product => {
+        this.product = product;
+        this.newProduct = JSON.parse(JSON.stringify(this.product));
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  }
+
+  edit() {
+    const fragment = utilities.filterUntouchedProperties(this.product, this.newProduct);
+    console.log(fragment);
+    this.api
+      .edit(`products/${this.params.product_id}`, fragment)
+      .then(success => notify().log('Successfully Updated'))
+      .catch(err => console.log(err));
+  }
+
+  swap(from, to) {
+    console.log(from, to);
+    const temp = this.newProduct.gallery[to];
+    this.newProduct.gallery[to] = this.newProduct.gallery[from];
+    this.newProduct.gallery[from] = temp;
+    console.log(this.newProduct.gallery);
+    console.log(this.product.gallery);
+  }
+
+  cancel(property) {
+    this.newProduct[property] = this.product[property];
+  }
 }
