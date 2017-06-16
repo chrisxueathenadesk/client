a57fee9044626c5eb282908fe62b919bd490fe78
AFM Sayem <afm.sayem@gmail.com>
2017-06-16 16:02:39 +0800

    chore(tracking): disable tracking in dev environment

--- aurelia_project/environments/dev.js
+++ aurelia_project/environments/dev.js
@@ -4,5 +4,5 @@ export default {
   base: 'http://192.168.1.156:3000/',
   stripe_key: 'pk_test_y6c6G7emK6M5t1tfURhHiWQz',
   ga_tracking_id: 'UA-101140118-1',
-  ga_tracking_enabled: true
+  ga_tracking_enabled: false
 };
