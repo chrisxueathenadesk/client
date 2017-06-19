89aa99f4f2f2fcd82e1b8bbfdc4e410f083ea858
AFM Sayem <afm.sayem@gmail.com>
2017-06-19 12:37:02 +0800

    chore(adwords): configure adwords

--- aurelia_project/environments/dev.js
+++ aurelia_project/environments/dev.js
@@ -4,5 +4,14 @@ export default {
   base: 'http://192.168.1.156:3000/',
   stripe_key: 'pk_test_y6c6G7emK6M5t1tfURhHiWQz',
   ga_tracking_id: 'UA-101140118-1',
-  ga_tracking_enabled: false
+  ga_tracking_enabled: false,
+  adwordtracking: true,
+  adwords: {
+    google_conversion_id: 873334207,
+    google_conversion_language: 'en',
+    google_conversion_format: '3',
+    google_conversion_color: 'ffffff',
+    google_conversion_currency: 'SGD',
+    google_remarketing_only: false
+  }
 };
--- aurelia_project/environments/prod.js
+++ aurelia_project/environments/prod.js
@@ -4,5 +4,14 @@ export default {
   base: 'https://api.novelship.com/',
   stripe_key: 'pk_live_ZSoVkfqE1IhCf6OJP9HtScJ4',
   ga_tracking_id: 'UA-101140118-1',
-  ga_tracking_enabled: true
+  ga_tracking_enabled: true,
+  adwordtracking: true,
+  adwords: {
+    google_conversion_id: 873334207,
+    google_conversion_language: 'en',
+    google_conversion_format: '3',
+    google_conversion_color: 'ffffff',
+    google_conversion_currency: 'SGD',
+    google_remarketing_only: false
+  }
 };
