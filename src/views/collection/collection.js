d444018b2866bdc9d8e783c92de97aff25f293fb
AFM Sayem <afm.sayem@gmail.com>
2017-06-30 16:24:48 +0800

    feat(collection): collection view with styling

--- src/views/collection/collection.js
+++ src/views/collection/collection.js
@@ -4,12 +4,22 @@ import {Api} from '~/services/api';
 @inject(Api)
 export class CollectionView {
   collectionproducts = {};
+  collection = {
+    page: {
+      size: 20
+    }
+  };
 
   constructor(api) {
     this.api = api;
   }
 
   activate(param) {
+    this.api
+      .fetch(`collections/${param.collection_id}`)
+      .then(collection => this.collection.data = collection)
+      .catch(err => this.collection.error = err);
+
     this.api
       .fetch(`collections/${param.collection_id}/collectionproducts`, {include: ['product']})
       .then(products => {
